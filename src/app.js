c02ff07c2e6b1adfabad5c525108c28ef7db6d04
kovalforpublic <kovalforpublic@gmail.com>
2020-04-14 13:41:08 +0300

    final rewrite, random password with all necessary symbol types

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// const formHandler = new FormHandler('.length');
-
 const className = '.length';
 const history = [];
 const lengthLabel = document.querySelector(className + ' > label');
@@ -9,53 +7,92 @@ const generateButton = document.querySelector(className + ' > button');
 const copyInput = document.querySelector('.password > input');
 const copyButton = document.querySelector('.password > button');
 
+const symbolTypes = ['lowCaseChars', 'upperCaseChars', 'numbers', 'specialSymbols'];
 const lowCaseChars = 'abcdefghijklmnopqrstuvwxyz';
 const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789';
 const specialSymbols = '!#$%&()*+,-./:;<=>?@[\]^_{|}~';
 
+const regexes = {
+    lowCaseChars: /[a-z]/,
+    upperCaseChars: /[A-Z]/,
+    numbers: /\d/,
+    symbols: /[!#$%&()\*\+,-.\/:;<=>?@\[\]\^_{|}~]/,
+    // symbols: /[!@#$%^&*)(+=._-]/, // alternative just in case
+};
+
 function generatePassword(length) {
     return new Promise(function(resolve) {
+        let counter = length;
         let password = '';
-        let currentType = 'lowCaseChars';
-        while (length) {
-            switch (currentType) {
-                case 'lowCaseChars':
-                    password += getLowCaseChar();
-                    currentType = 'upperCaseChars';
-                    break;
-                case 'upperCaseChars':
-                    password += getUpperCaseChar();
-                    currentType = 'numbers';
-                    break;
-                case 'numbers':
-                    password += getNumber();
-                    currentType = 'specialSymbols';
-                    break;
-                case 'specialSymbols':
-                    password += getSpecialSymbol();
-                    currentType = 'lowCaseChars';
-                    break;
+
+        if (length === 4) {
+            password += getLowCaseChar();
+            password += getUpperCaseChar();
+            password += getNumber();
+            password += getSpecialSymbol();
+            password = randomise(password);
+        } else {
+            while (isDuplicate(password)) {
+                password = '';
+                counter = length;
+                while (counter) {
+
+                    if (counter <= 3) {
+                        currentType = whichTypeMissed(password);
+                    } else {
+                        currentType = getRandomType();
+                    }
+
+                    switch (currentType) {
+                        case 'lowCaseChars':
+                        password += getLowCaseChar();
+                        break;
+                        case 'upperCaseChars':
+                        password += getUpperCaseChar();
+                        break;
+                        case 'numbers':
+                        password += getNumber();
+                        break;
+                        case 'specialSymbols':
+                        password += getSpecialSymbol();
+                        break;
+                    }
+                    counter--;
+                }
             }
-            length--;
         }
-        resolve(randomise(password));
+        resolve(password);
     })
 }
 
-function updatePwd(pwd) {
-    copyInput.value = pwd;
+function getRandomType() {
+    return symbolTypes[Math.floor(Math.random() * symbolTypes.length)];
 }
+function whichTypeMissed(password) {
+    const isLow = regexes.lowCaseChars.test(password);
+    const isUp = regexes.upperCaseChars.test(password);
+    const isNum = regexes.numbers.test(password);
+    const isSpecial = regexes.symbols.test(password);
 
-function randomise(string) {
-    const arr = string.split('');
-    let x;
-    for (let i = arr.length - 1; i > 0; i--) {
-        x = Math.floor(Math.random() * (i + 1));
-        // destructuring, [a, b] = [b, a]
-        [arr[x], arr[i]] = [arr[i], arr[x]]
+    if (!isLow) {
+        return 'lowCaseChars';
+    } else if (!isUp) {
+        return 'upperCaseChars';
+    } else if (!isNum) {
+        return 'numbers';
+    } else if (!isSpecial) {
+        return 'specialSymbols';
+    } else {
+        return getRandomType();
     }
-    return arr.join('');
+}
+function isDuplicate(password) {
+    if (password === '') {
+        return true;
+    } else {
+        return history.includes(password);
+    }
 }
 
 function getLowCaseChar() {
@@ -73,18 +110,32 @@ function getSpecialSymbol() {
 function getRandomIndex(characters) {
     return Math.floor(Math.random() * characters.length);
 }
+
+function randomise(string) {
+    const arr = string.split('');
+    let x;
+    for (let i = arr.length - 1; i > 0; i--) {
+        x = Math.floor(Math.random() * (i + 1));
+        // destructuring, [a, b] = [b, a]
+        [arr[x], arr[i]] = [arr[i], arr[x]]
+    }
+    return arr.join('');
+}
+
+function updatePwd(pwd) {
+    copyInput.value = pwd;
+}
+
 // event bindings
-(function() {
-    copyButton.addEventListener('click', () => {
-        navigator.clipboard.writeText(input.value);
-    })
-    lengthInput.addEventListener('input', (e) => {
-        lengthLabel.innerHTML = e.target.value;
-    });
-    generateButton.addEventListener('click', async () => {
-        const length = Number(lengthLabel.innerHTML);
-        const password = await generatePassword(length);
-        history.push(password);
-        updatePwd(password);
-    })
-})();
+copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(copyInput.value);
+})
+lengthInput.addEventListener('input', (e) => {
+    lengthLabel.innerHTML = e.target.value;
+});
+generateButton.addEventListener('click', async () => {
+    const length = Number(lengthLabel.innerHTML);
+    const password = await generatePassword(length);
+    history.push(password);
+    updatePwd(password);
+})
